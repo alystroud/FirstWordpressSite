9f1d743b6af03afc396aea77523ba1307c72ef7e
alystroud <alygrace@hotmail.co.uk>
2019-01-13 21:07:52 +0000

    Changed nav background colour. Added title to page element.

--- wp-content/themes/startwordpress/content.php
+++ wp-content/themes/startwordpress/content.php
@@ -1,17 +1,16 @@
 <div class="blog-post">
-	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> |
-    <a href="<?php comments_link(); ?>">
-      <?php
-      printf( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
-    </a></p>
-
-    <?php if ( has_post_thumbnail() ) {?>
+	  <?php if ( has_post_thumbnail() ) {?>
    	<div class="row">
    		<div class="col-md-4 thumbnail">
    			<?php	the_post_thumbnail('thumbnail'); ?>
    		</div>
    		<div class="col-md-6">
+				<h4 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+				<p class="blog-post-meta"><?php the_date(); ?> |
+			    <a href="<?php comments_link(); ?>">
+			      <?php
+			      printf( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
+			    </a></p>
    			<?php the_excerpt(); ?>
    		</div>
    	</div>
--- wp-content/themes/startwordpress/page.php
+++ wp-content/themes/startwordpress/page.php
@@ -2,7 +2,7 @@
 
 	<div class="row">
 		<div class="col-sm-12">
-
+			<h3 class="blog-post-title"><?php the_title(); ?></h3>
 			<?php
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
 
